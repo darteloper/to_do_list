4a0c78f918170e96786a7bc3ec5cfcdebab9b67b
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-02 22:03:10 -0400

    add listing function

---
+++ web/utils.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+import 'script.dart';
+
+void printList() {
+  var listTask = querySelector('#list');
+  listTask.innerHtml = '';
+  for(Task task in list){
+    var newToDo = LIElement();
+    newToDo.text = '${task.printData()}';
+    listTask.children.add(newToDo);
+  }
+}
