7497872871633794e499b538b4120267025149be
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-11-25 14:25:00 -0400

    Improve functionalities

--- web/utils.dart
+++ web/utils.dart
@@ -4,7 +4,7 @@ import 'script.dart';
 void printList() {
   var listTask = querySelector('#list');
   listTask.innerHtml = '';
-  for(Task task in list){
+  for (Task task in list) {
     var newToDo = LIElement();
     newToDo.text = '${task.printData()}';
     listTask.children.add(newToDo);
